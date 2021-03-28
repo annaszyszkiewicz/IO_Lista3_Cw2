7925f40d5d165c2a3c23f28d19f04bad1c3d6e98
annaszyszkiewicz <250386@student.pwr.edu.pl>
2021-03-28 13:34:58 +0200

    Initial commit

---
+++ src/test/java/edu/pwr/IO_Lista3_Cw2/IoLista3Cw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pwr.IO_Lista3_Cw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoLista3Cw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
